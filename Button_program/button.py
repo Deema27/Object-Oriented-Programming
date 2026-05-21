a2d344d8d1d9a30a3502505ca1b728dd6855eff5
Deema27 <deemaalbadawi123@gmail.com>
2026-05-21 16:53:15 -0400

    Implement SimpleButton class for mouse interaction
    Defines a SimpleButton class using a state machine to handle mouse interaction and button clicks in pygame.

--- Button_program/button.py
+++ Button_program/button.py
@@ -1,52 +1,57 @@
-# SimpleButton class
-#
-# Uses a "state machine" approach
-#
+''' Description: 
+defines a SimpleButton class using a state machine 
+to handle mouse interaction and button clicks in pygame '''
 
+# 1 - Import packages
 import pygame
 from pygame.locals import *
 
+# SimpleButton class using a state machine for interaction
 class SimpleButton():
-    # Used to track the state of the button
-    STATE_IDLE = 'idle' # button is up, mouse not over button
-    STATE_ARMED = 'armed' # button is down, mouse over button
-    STATE_DISARMED = 'disarmed' # clicked down on button, rolled off
-        
+
+    # Button states
+    STATE_IDLE = 'idle'          # button is not pressed
+    STATE_ARMED = 'armed'        # button is pressed and mouse is over it
+    STATE_DISARMED = 'disarmed'  # button was pressed but mouse moved off
+
+    # 2 - Initialize button
     def __init__(self, window, loc, up, down):
         self.window = window
         self.loc = loc
         self.surfaceUp = pygame.image.load(up)
         self.surfaceDown = pygame.image.load(down)
 
-        # Get the rect of the button (used to see if the mouse is over the button)
+        # Rectangle used for mouse collision detection
         self.rect = self.surfaceUp.get_rect()
         self.rect[0] = loc[0]
         self.rect[1] = loc[1]
 
         self.state = SimpleButton.STATE_IDLE
 
+    # 3 - Handle mouse events and update button state
     def handleEvent(self, eventObj):
-        # This method will return True if user clicks the button.
-        # Normally returns False.
 
+        # Only respond to mouse events
         if eventObj.type not in (MOUSEMOTION, MOUSEBUTTONUP, MOUSEBUTTONDOWN):
-            # The button only cares about mouse-related events
             return False
 
         eventPointInButtonRect = self.rect.collidepoint(eventObj.pos)
 
+        # Idle state (not pressed)
         if self.state == SimpleButton.STATE_IDLE:
-            if (eventObj.type == MOUSEBUTTONDOWN) and eventPointInButtonRect:
+            if eventObj.type == MOUSEBUTTONDOWN and eventPointInButtonRect:
                 self.state = SimpleButton.STATE_ARMED
 
+        # Pressed state
         elif self.state == SimpleButton.STATE_ARMED:
-            if (eventObj.type == MOUSEBUTTONUP) and eventPointInButtonRect:
+            if eventObj.type == MOUSEBUTTONUP and eventPointInButtonRect:
                 self.state = SimpleButton.STATE_IDLE
-                return True  # clicked!
+                return True
 
-            if (eventObj.type == MOUSEMOTION) and (not eventPointInButtonRect):
+            if eventObj.type == MOUSEMOTION and not eventPointInButtonRect:
                 self.state = SimpleButton.STATE_DISARMED
 
+        # Dragged off state
         elif self.state == SimpleButton.STATE_DISARMED:
             if eventPointInButtonRect:
                 self.state = SimpleButton.STATE_ARMED
@@ -55,10 +60,10 @@ class SimpleButton():
 
         return False
 
+    # 4 - Draw button
     def draw(self):
-        # Draw the button's current appearance to the window
+
         if self.state == SimpleButton.STATE_ARMED:
             self.window.blit(self.surfaceDown, self.loc)
-
-        else:  # IDLE or DISARMED
+        else:
             self.window.blit(self.surfaceUp, self.loc)
