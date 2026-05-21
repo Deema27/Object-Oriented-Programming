a05299f9e2336bf674cc94c04b41b58bfb261d9e
Deema27 <deemaalbadawi123@gmail.com>
2026-05-21 16:34:28 -0400

    Update and rename PygamePractice8.py to pygame_mouse_keyboard_movement.py

--- pygame_mouse_keyboard_movement.py
+++ pygame_mouse_keyboard_movement.py
@@ -1,3 +1,7 @@
+''' Description: 
+creates a pygame program where a ball can be 
+moved with arrow keys and repositioned by clicking objects on the screen '''
+
 # 1 - Import packages
 import pygame, sys, random
 
@@ -14,51 +18,74 @@ pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 
-# 4 - Load assets: image(s), sound(s),  etc.
+# 4 - Load assets: image(s), sound(s), etc.
 ballImage = pygame.image.load('images/ball.png')
 
 # 5 - Initialize variables
+#Set initial ball position
 ballX = 50
 ballY = 350
+
+#Create rectangle for collision detection
 ballRect = pygame.Rect(ballX, ballY, 100, 100)
 
+#Set circle position and size
 circlex = 150
 circley = 50
 circle_rad = 30
+
+#Create rectangle for circle collision detection
 circlerect = pygame.Rect(circlex - circle_rad, circley - circle_rad, circle_rad * 2, circle_rad * 2)
 
-# 6 - Loop forever
+# 6 - Main loop
 while True:
-    # 7 - Check for and handle events
+
+    # 7 - Handle events
     for event in pygame.event.get():
+
+        #Exit program when window is closed
         if event.type == pygame.QUIT:           
             pygame.quit()  
             sys.exit()
+
+        #Handle mouse clicks
         if event.type == pygame.MOUSEBUTTONDOWN:
+
+            #If ball is clicked, move it to random position
             if ballRect.collidepoint(event.pos):
                 ballX = random.randrange(580)
                 ballY = random.randrange(380)
                 ballRect = pygame.Rect(ballX, ballY, 100, 100)
+
+            #If circle is clicked, move it to random position
             if circlerect.collidepoint(event.pos):
                 circlex = random.randrange(490)
                 circley = random.randrange(430)
                 circlerect = pygame.Rect(circlex - circle_rad, circley - circle_rad, circle_rad * 2, circle_rad * 2)
-                
 
-        
-     # 8 - Do any "per frame" actions
+    # 8 - Per frame updates (keyboard movement)
     keyPressedTuple = pygame.key.get_pressed()
-    if keyPressedTuple[pygame.K_LEFT]:  # moving left
-        ballX = ballX - N_PIXELS_TO_MOVE
-    if keyPressedTuple[pygame.K_RIGHT]:  # moving right
-        ballX = ballX + N_PIXELS_TO_MOVE
-    if keyPressedTuple[pygame.K_UP]:  # moving up
-        ballY = ballY - N_PIXELS_TO_MOVE
-    if keyPressedTuple[pygame.K_DOWN]:  # moving down
-        ballY = ballY + N_PIXELS_TO_MOVE            
+
+    #Move ball left
+    if keyPressedTuple[pygame.K_LEFT]:
+        ballX -= N_PIXELS_TO_MOVE
+
+    #Move ball right
+    if keyPressedTuple[pygame.K_RIGHT]:
+        ballX += N_PIXELS_TO_MOVE
+
+    #Move ball up
+    if keyPressedTuple[pygame.K_UP]:
+        ballY -= N_PIXELS_TO_MOVE
+
+    #Move ball down
+    if keyPressedTuple[pygame.K_DOWN]:
+        ballY += N_PIXELS_TO_MOVE
+
+    #Update ball rectangle position
     ballRect = pygame.Rect(ballX, ballY, 100, 100)
 
-
+    #Keep ball inside screen boundaries
     if ballX < 0:
         ballX = 0
     if ballX + 100 >= WINDOW_WIDTH:
@@ -68,22 +95,16 @@ while True:
     if ballY + 100 >= WINDOW_HEIGHT:
         ballY = WINDOW_HEIGHT - 100
 
-
-    
-   
-    # 9 - Clear the window
+    # 9 - Clear screen
     window.fill(COLOR2)
 
-
-    
-    # 10 - Draw all window elements
-    circlerect = pygame.draw.circle(window, COLOR1, (circlex, circley), circle_rad, 0) # filled
-    pygame.draw.rect(window, COLOR1, (400, 300, 100, 100), 3) # 3 pixel edge
+    # 10 - Draw shapes and image
+    circlerect = pygame.draw.circle(window, COLOR1, (circlex, circley), circle_rad, 0)
+    pygame.draw.rect(window, COLOR1, (400, 300, 100, 100), 3)
     window.blit(ballImage, (ballX, ballY))
 
-
-
-    # 11 - Update the window
+    # 11 - Update display
     pygame.display.update()
-    # 12 - Slow things down a bit
-    clock.tick(FRAMES_PER_SECOND)  # make pygame wait
+
+    # 12 - Control frame rate
+    clock.tick(FRAMES_PER_SECOND)
